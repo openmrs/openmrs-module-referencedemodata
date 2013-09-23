c33666aaa7054cb349792e190fc622a77b80b88f
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2013-09-23 14:33:35 +0200

    RA-182 Corrected roles

--- api/src/main/java/org/openmrs/module/referencedemodata/ReferenceDemoDataConstants.java
+++ api/src/main/java/org/openmrs/module/referencedemodata/ReferenceDemoDataConstants.java
@@ -24,9 +24,9 @@ public class ReferenceDemoDataConstants {
 	public static final String CLERK_USER_UUID = "4882dd60-0508-11e3-8ffd-0800200c9a66";
 	public static final String NURSE_USER_UUID = "564b2790-0508-11e3-8ffd-0800200c9a66";
 	public static final String DOCTOR_USER_UUID = "67bfd7f0-0508-11e3-8ffd-0800200c9a66";
-	public static final String NURSE_ROLE_UUID = "a596c6fe-4c83-401f-9b71-86b140b4261a";
-	public static final String CLERK_ROLE_UUID = "aea81c49-5a27-4960-9a44-9ce9fe607c41";
-	public static final String DOCTOR_ROLE_UUID = "5bb62b1d-bd7b-4171-ade9-2c87f9876b87";
+	public static final String NURSE_ROLE_UUID = "9d92d41e-e0f8-4e8e-8f1d-cf43eefca86b";
+	public static final String CLERK_ROLE_UUID = "59dd27aa-887a-468d-9793-af0b490ec7b9";
+	public static final String DOCTOR_ROLE_UUID = "339cafe1-acd6-4bd5-8d28-15cc493d2607";
 	public static final String CLERK_PROVIDER_ROLE_UUID = "87d2122b-691c-4e06-9366-111f5ab2f526";
 	public static final String NURSE_PROVIDER_ROLE_UUID = "da13814f-f560-46df-8bb2-219e146c2811";
 	public static final String DOCTOR_PROVIDER_ROLE_UUID = "c110f9bc-c65f-44a2-a028-2af7e8fff534";
